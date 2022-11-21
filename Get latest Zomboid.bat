29cc007fee13757797a87c80065dd0e0824ea09e
Alex Plummer <darkguile86@gmail.com>
2022-11-21 14:00:22 +0000

    Save 20221121.140022

--- Get latest Zomboid.bat
+++ Get latest Zomboid.bat
@@ -1,5 +1,6 @@
 cd ../Zomboid/Saves/Multiplayer
 git pull
+@echo off 
 echo 'Finished, you can now close this message'
 pause 
 exit
--- Save Zomboid.bat
+++ Save Zomboid.bat
@@ -2,6 +2,7 @@ cd ../Zomboid/Saves/Multiplayer
 git add -A
 git commit -m "Save %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
 git push
+@echo off 
 echo 'Finished, you can now close this message'
 pause 
 exit
